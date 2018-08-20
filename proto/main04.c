0c8c6140f555239afeeb941c1abd145dceea5fb1
Stephane VIEIRA <stvieira@e2r9p1.42.fr>
2018-08-20 02:23:02 +0200

    update main04.c

--- proto/main04.c
+++ proto/main04.c
@@ -26,7 +26,7 @@ int		main(void)
 
 /********************************************/
   // EX01 ft_recursive_factorial
-
+/*
   printf("%s\n", "13! = 0");
 	printf("%d\n", ft_recursive_factorial(13));
 	printf("%s\n", "1! = 1");
@@ -35,10 +35,10 @@ int		main(void)
 	printf("%d\n", ft_recursive_factorial(5));
 	printf("%s\n", "9! = 362880");
 	printf("%d", ft_recursive_factorial(9));
-
+*/
 /********************************************/
   // EX02 ft_iterative_power
-
+/*
   printf("%s\n", "1 power 0 = 1");
   printf("%d\n", ft_iterative_power(5,13));
   printf("%s\n", "1 power 1 = 1");
@@ -47,10 +47,10 @@ int		main(void)
   printf("%d\n", ft_iterative_power(5,4));
   printf("%s\n", "9 power 1 = 9");
   printf("%d\n", ft_iterative_power(9,1));
-
+*/
 /********************************************/
   // EX03 ft_recursive_power
-
+/*
   printf("%s\n", "1 power 0 = 1");
   printf("%d\n", ft_recursive_power(1,0));
   printf("%s\n", "1 power 1 = 1");
@@ -59,10 +59,10 @@ int		main(void)
   printf("%d\n", ft_recursive_power(5,4));
   printf("%s\n", "9 power 1 = 9");
   printf("%d\n", ft_recursive_power(9,1));
-
+*/
 /********************************************/
   // EX04 ft_fibonacci
-
+/*
   printf("%s\n", "Negatif return -1");
   printf("%d\n", ft_fibonacci(-2));
   printf("%d\n", ft_fibonacci(-3));
@@ -80,10 +80,10 @@ int		main(void)
   printf("%d\n", ft_fibonacci(10));
   printf("%d\n", ft_fibonacci(20));
   printf("%d\n", ft_fibonacci(30));
-
+*/
 /********************************************/
   // EX05 ft_sqrt
-
+/*
     printf("%s\n", "9 racine carré = 3");
   	printf("%d\n", ft_sqrt(9));
   	printf("%s\n", "5 racine carré = 0");
@@ -92,10 +92,10 @@ int		main(void)
   	printf("%d\n", ft_sqrt(1));
     printf("%s\n", "0 racine carré = 0");
     printf("%d\n", ft_sqrt(0));
-
+*/
 /********************************************/
   // EX06 ft_is_prime
-
+/*
     printf("%s\n", "9 return 0");
   	printf("%d\n", ft_is_prime(9));
   	printf("%s\n", "7 return 1");
@@ -104,17 +104,17 @@ int		main(void)
   	printf("%d\n", ft_is_prime(-42));
     printf("%s\n", "Int return 1");
     printf("%d\n", ft_is_prime(2147483647));
-
+*/
 /********************************************/
   // EX07 ft_find_next_prime
-
+/*
     printf("%s\n", "9 return 11");
   	printf("%d\n", ft_find_next_prime(9));
   	printf("%s\n", "7 return 7");
   	printf("%d\n", ft_find_next_prime(7));
   	printf("%s\n", "-42 return 2 car premier nombre premier");
   	printf("%d\n", ft_find_next_prime(-42));
-
+*/
 /********************************************/
 
 	return (0);
